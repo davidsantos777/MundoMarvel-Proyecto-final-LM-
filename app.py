f0b05370a42dca264e7fa875aaea587a768c2446
David Santos <seven_dvd@hotmail.com>
2018-05-21 22:33:38 +0200

    añadido app.py

---
+++ app.py
@@ -0,0 +1,44 @@
+#_*_ coding: utf-8 _*_
+
+from flask import Flask, render_template, request
+import hashlib
+import requests
+import os
+import json
+
+public = '5404f8cbf841577db6f94fb3a3098d51'
+private = 'a21215a318c6fb1eeb9e5763e08a9c7352317055'
+ts = '1'
+
+app = Flask(__name__)
+
+port=os.environ["PORT"]
+
+hash = hashlib.md5((ts + private + public).encode()).hexdigest()
+
+base = "https://gateway.marvel.com/v1/public/"
+
+@app.route('/')
+def inicio():
+  return render_template('index.html')
+
+@app.route('/busqueda', methods = ["get", "post"])
+def busqueda():
+  lista = []
+  if request.method == "GET":
+    return render_template('index.html')
+  else:
+    nombre = raw_input("Dime el titulo de un comic: ")
+    payload = {'apikey': public,'ts': ts,'hash': hash,'title': nombre}
+    r = requests.get(base + 'comics', params= payload)
+    if r.status_code == 200:
+      results = r.json()
+      lista.append(results)
+      return render_template('index.html', datos = lista)
+
+
+app.run('0.0.0.0',int(port), debug=True)
+
+
+
+                                  
