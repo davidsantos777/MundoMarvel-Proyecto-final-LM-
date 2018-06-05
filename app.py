06f18f52522934ac4613211f6ec41ecbd6808b5e
David Santos <35598098+davidsantos777@users.noreply.github.com>
2018-06-06 01:36:01 +0200

    Add files via upload
    Actualizo app.py con más opciones de búsqueda

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ private = 'a21215a318c6fb1eeb9e5763e08a9c7352317055'
 ts = '1'
 
 app = Flask(__name__)
-port = os.environ['PORT']
 
 hash = hashlib.md5((ts + private + public).encode()).hexdigest()
 
@@ -21,8 +20,12 @@ base = "https://gateway.marvel.com/v1/public/"
 def inicio():
   return render_template('index.html')
 
-@app.route('/busqueda', methods = ["get", "post"])
+@app.route('/busqueda')
 def busqueda():
+  return render_template('landing.html')
+
+@app.route('/busqueda_personalizada', methods = ["get", "post"])
+def busqueda_personalizada():
   if request.method == "GET":
     return render_template('index.html')
   else:
@@ -36,13 +39,46 @@ def busqueda():
         results = r.json()
         for i in results['data']['results']:
           lista.append({'Id': i["id"], 'Titulo': i['title'], 'Sinopsis': i['description']})
+
     elif tipo == "eventos":
       payload = {'apikey': public,'ts': ts,'hash': hash,'title': nombre}
-      r = requests.get(base + 'comics', params= payload)
+      r = requests.get(base + 'events', params= payload)
       if r.status_code == 200:
         results = r.json()
         for i in results['data']['results']:
           lista.append({'Id': i["id"], 'Titulo': i['title'], 'Sinopsis': i['description']})
-    return render_template('index.html', datos = lista)
 
-app.run('0.0.0.0', int(port), debug = True)
+    elif tipo == "personajes":
+      payload = {'apikey': public,'ts': ts,'hash': hash,'name': nombre}
+      r = requests.get(base + 'characters', params= payload)
+      if r.status_code == 200:
+        results = r.json()
+        for i in results['data']['results']:
+          lista.append({'Id': i["id"], 'Nombre': i['name'], 'Biografía': i['description']})
+
+    elif tipo == "creadores":
+      payload = {'apikey': public,'ts': ts,'hash': hash,'firstName': nombre}
+      r = requests.get(base + 'creators', params= payload)
+      if r.status_code == 200:
+        results = r.json()
+        for i in results['data']['results']:
+          lista.append({'Id': i["id"], 'Nombre': i['firstName'], 'Sinopsis': i['description']})
+
+    elif tipo == "series":
+      payload = {'apikey': public,'ts': ts,'hash': hash,'title': nombre}
+      r = requests.get(base + 'series', params= payload)
+      if r.status_code == 200:
+        results = r.json()
+        for i in results['data']['results']:
+          lista.append({'Id': i["id"], 'Titulo': i['title'], 'Sinopsis': i['description']})
+          
+    elif tipo == "historias":
+      payload = {'apikey': public,'ts': ts,'hash': hash,'title': nombre}
+      r = requests.get(base + 'stories', params= payload)
+      if r.status_code == 200:
+        results = r.json()
+        for i in results['data']['results']:
+          lista.append({'Id': i["id"], 'Titulo': i['title'], 'Sinopsis': i['description']})
+    return render_template('landing.html', datos = lista)
+
+app.run()
